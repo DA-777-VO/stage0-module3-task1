81582e1c716b2b286fd6114fc418fc652a284c79
David Apakin <apakin.david@gmail.com>
2023-10-23 21:59:33 +0400

    Exercise 3.1 v2

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -3,7 +3,8 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Dota 2" + "\n" + "is" + "\n" + "a" + "\n" + "good" + "\n" + "game");
-
+        System.out.println("java");
+        System.out.println("is");
+        System.out.println("smth");
     }
 }
